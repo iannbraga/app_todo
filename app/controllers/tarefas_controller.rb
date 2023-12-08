4a3ffb7d3d291f8e39fb5d225f16d86ea25bdff6
iann.braga <bragaprestes@gmail.com>
2023-12-08 17:15:00 -0300

    Add crud de tarefa

--- app/controllers/tarefas_controller.rb
+++ app/controllers/tarefas_controller.rb
@@ -6,4 +6,31 @@ class TarefasController < ApplicationController
     def new
         @tarefa = Tarefa.new
     end
+
+    def create
+        @tarefa = Tarefa.new(tarefa_params)
+        if @tarefa.save
+            redirect_to tarefas_url
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @tarefa = Tarefa.find(params[:id])
+        @tarefa.destroy
+        redirect_to tarefas_url
+    end
+
+    def tarefa_concluida
+        @tarefa = Tarefa.find(params[:id])
+        @tarefa.update(concluida: true)
+        redirect_to tarefas_url
+    end
+
+    private 
+
+    def tarefa_params
+        params.require(:tarefa).permit(:titulo, :descricao, :concluida)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :tarefas
+  resources :tarefas 
+  get 'tarefa_concluida/:id', to: 'tarefas#tarefa_concluida', as: :tarefa_concluida
   # Defines the root path route ("/")
   root "tarefas#index"
 end
