03cf8c3ee5ff6708c98b38cc1c269b0c615b7dff
iann.braga <bragaprestes@gmail.com>
2023-12-11 18:29:17 -0300

    Crud completo, ajustes visuais, add modal

--- app/controllers/tarefas_controller.rb
+++ app/controllers/tarefas_controller.rb
@@ -1,4 +1,5 @@
 class TarefasController < ApplicationController
+    before_action :set_tarefa, only: %i[ edit update show destroy tarefa_concluida ]
     def index
         @tarefas = Tarefa.order(:desc)
     end
@@ -16,19 +17,35 @@ class TarefasController < ApplicationController
         end
     end
 
+    def edit
+    end
+
+    def update
+        if @tarefa.update(tarefa_params)
+            redirect_to tarefas_url
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def show
+    end
+
     def destroy
-        @tarefa = Tarefa.find(params[:id])
         @tarefa.destroy
         redirect_to tarefas_url
     end
 
     def tarefa_concluida
-        @tarefa = Tarefa.find(params[:id])
         @tarefa.update(concluida: true)
         redirect_to tarefas_url
     end
 
     private 
+
+    def set_tarefa
+        @tarefa = Tarefa.find(params[:id])
+    end
 
     def tarefa_params
         params.require(:tarefa).permit(:titulo, :descricao, :concluida)
